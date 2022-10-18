574bd919c923ced78f5b044ab2637ae4d82b7e74
Mathis Clotteau <mclotteau@outlook.fr>
2022-10-18 09:05:38 +0200

    mistake corrected

--- queries.sql
+++ queries.sql
@@ -1,8 +1,20 @@
-SELECT c.*, oc.id_order,p.bill_amout, p.payment_date, r.reservation_date, r.reservation_time, r.nb_customer
+SELECT c.*, oc.id_order,p.bill_amount, p.payment_date, r.reservation_date, r.reservation_time, r.nb_customer
 FROM customer c
 JOIN order_customer oc ON oc.id_customer=c.id_customer
 JOIN payment p ON oc.id_order=p.id_order
 JOIN reservation r ON r.id_customer=c.id_customer
-WHERE c.id_customer='1';
+WHERE c.id_customer='2';
 
+SELECT c.customer_name, r.reservation_date,r.reservation_time,r.nb_customer
+FROM customer c
+JOIN reservation r ON r.id_customer=c.id_customer;
 
+SELECT c.customer_name,r.nb_customer,p.bill_amount,p.payment_date
+FROM customer c
+JOIN reservation r ON c.id_customer=r.id_customer
+JOIN payment p ON p.id_customer=c.id_customer;
+
+SELECT c.customer_name,c.id_table,oc.nb_food
+FROM customer c
+JOIN order_customer oc ON c.id_customer=oc.id_customer
+WHERE oc.nb_food>10;
--- seed.sql
+++ seed.sql
@@ -2,11 +2,12 @@ INSERT INTO customer VALUES ('1','Mathis','0643759990','table VIP');
 INSERT INTO customer VALUES ('2','Alexandre','0777613901','table 1');
 
 INSERT INTO order_customer VALUES ('1','1','7','entry');
-INSERT INTO order_customer VALUES ('3','1','5','meal');
+INSERT INTO order_customer VALUES ('3','1','2','meal');
 INSERT INTO order_customer VALUES ('2','2','14','full order');
 
-INSERT INTO payment VALUES ('1','1','250','10 Janvier 2022');
-INSERT INTO payment VALUES ('2','2','200','29 Juillet 2021');
+INSERT INTO payment VALUES ('1','1','1','200','10 Janvier 2022');
+INSERT INTO payment VALUES ('3','3','1','100','10 Janvier 2022');
+INSERT INTO payment VALUES ('2','2','2','200','29 Juillet 2021');
 
 INSERT INTO reservation VALUES ('1','9 Janvier 2022','8pm','5','1');
 INSERT INTO reservation VALUES ('2','25 Juillet 2021','7.30pm','12','2');
